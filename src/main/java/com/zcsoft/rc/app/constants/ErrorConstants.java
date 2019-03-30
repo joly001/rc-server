f5b43e37fb423362d156fc2dc2564090d19c5f07
joly001 <joly_huang@163.com>
2019-03-30 14:12:15 +0800

    init

--- src/main/java/com/zcsoft/rc/app/constants/ErrorConstants.java
+++ src/main/java/com/zcsoft/rc/app/constants/ErrorConstants.java
@@ -7,7 +7,7 @@ public interface ErrorConstants {
      */
     String USER_INCORRECT_USERNAME_OR_PASSWORD = "003000";
     /**
-     * 账号已登陆另一台设备，请重新登录
+     * 账号已登录另一台设备，请重新登录
      */
     String USER_LOGIN_AT_OTHER_DEVICE = "003001";
 }
