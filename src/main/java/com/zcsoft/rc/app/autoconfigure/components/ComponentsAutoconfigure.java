d1d7f42ff862a4019912c748fc5e9cb65d5ecb1c
joly001 <joly_huang@163.com>
2019-04-01 10:44:21 +0800

    init

--- src/main/java/com/zcsoft/rc/app/autoconfigure/components/ComponentsAutoconfigure.java
+++ src/main/java/com/zcsoft/rc/app/autoconfigure/components/ComponentsAutoconfigure.java
@@ -1,8 +1,45 @@
 package com.zcsoft.rc.app.autoconfigure.components;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class ComponentsAutoconfigure {
+
+    @Bean(name="fileResourceHttpRequestHandler")
+    public ResourceHttpRequestHandler createFileResourceHttpRequestHandler(@Value("${user.photo.file.path}") String driverPhotoFilePath) {
+
+        List<Resource> locations = new ArrayList<>();
+        locations.add(new PathResource(driverPhotoFilePath));
+
+        ResourceHttpRequestHandler resourceHttpRequestHandler = new ResourceHttpRequestHandler();
+        resourceHttpRequestHandler.setLocations(locations);
+
+        return resourceHttpRequestHandler;
+    }
+
+    @Bean(name="simpleUrlHandlerMapping")
+    public SimpleUrlHandlerMapping createSimpleUrlHandlerMapping(ResourceHttpRequestHandler resourceHttpRequestHandler, ResourceHttpRequestHandler fileResourceHttpRequestHandler) {
+
+        Map<String, ResourceHttpRequestHandler> urlMap = new ManagedMap<>();
+        urlMap.put("/static/**", resourceHttpRequestHandler);
+        urlMap.put("/photo/**", fileResourceHttpRequestHandler);
+        urlMap.put("**/favicon.ico", resourceHttpRequestHandler);
+
+        SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+        simpleUrlHandlerMapping.setUrlMap(urlMap);
+
+        return simpleUrlHandlerMapping;
+    }
 
 }
