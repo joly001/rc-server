e5fd7c664528d75b93d8e5c037f8d770e0e9be62
joly001 <joly_huang@163.com>
2019-06-05 00:14:54 +0800

    init

--- src/main/webapp/static/map/js/pageConfig.js
+++ src/main/webapp/static/map/js/pageConfig.js
@@ -1,4 +1,5 @@
-var wsUrl = "ws://119.3.246.90:9090/rc-collectors";
+// var wsUrl = "ws://119.3.246.90:9090/rc-collectors";
+var wsUrl = "ws://119.3.246.90:8800/iserver/services/dataflow/dataflow/subscribe";
 var isLocaltion = window.location.href.indexOf('http') === -1
 var baseUrl = isLocaltion ? "http://119.3.246.90:9091" : "";
 var loginUrl = isLocaltion ? window.location.href : top.location.href.split('wisdom/info')[0];
